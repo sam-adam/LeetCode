8f9c1637fc27defff126ab88fcb03fe78cc69063
Samuel Adam Suhendra <samuel.suhendra@gmail.com>
2023-09-13 22:34:55 +0700

    Time: 403 ms (5%), Space: 9 MB (6.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
@@ -4,24 +4,26 @@ func lengthOfLongestSubstring(s string) int {
 	arr := strings.Split(s, "")
 	arrLen := len(arr)
 
-	for i := 0; i < arrLen; i++ {
-		chars := make(map[string]bool, 0)
+	for i := 0; i < arrLen - longest; i++ {
+		chars := make(map[string]int, 0)
 		length := 1
 
-		chars[arr[i]] = true
+		chars[arr[i]] = i
 
 		for j := i + 1; j < arrLen; j++ {
-			if _, exists := chars[arr[j]]; exists {
+			if dupIdx, exists := chars[arr[j]]; exists {
+				i = dupIdx
+
 				break
 			}
 
-			chars[arr[j]] = true
+			chars[arr[j]] = j
 			length++
 		}
 
 		if length > longest {
-					longest = length
-				}
+			longest = length
+		}
 	}
 
 	return longest
