c0e6969b7deda1be638416116602ce5afe2db36f
Samuel Adam Suhendra <samuel.suhendra@gmail.com>
2023-09-16 00:09:40 +0700

    Time: 11 ms (41.71%), Space: 4.7 MB (14.39%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.go
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -6,65 +6,41 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var rootNode *ListNode
-	var currentNode *ListNode
-
-	isInitial := true
-	carryOver := 0
-
-	for {
-		co, res := addNode(carryOver, l1, l2)
-		resNode := ListNode{
-			Val:  res,
-			Next: nil,
-		}
-
-		carryOver = co
-
-		if isInitial {
-			rootNode = &resNode
-			currentNode = rootNode
-			isInitial = false
-		} else {
-			currentNode.Next = &resNode
-			currentNode = currentNode.Next
-		}
-
-		if l1 != nil {
-			l1 = l1.Next
-		}
-
-		if l2 != nil {
-			l2 = l2.Next
-		}
-
-		if l1 == nil && l2 == nil {
-			if carryOver == 1 {
-				currentNode.Next = &ListNode{Val: carryOver}
-			}
-
-			break
-		}
-	}
-
-	return rootNode
+	return addNode(0, l1, l2)
 }
 
-func addNode(co int, l1 *ListNode, l2 *ListNode) (int, int) {
-	res := co
+func addNode(co int, l1 *ListNode, l2 *ListNode) *ListNode {
+    if l1 == nil && l2 == nil {
+        if co == 1 {
+            return &ListNode{Val: 1}
+        }
+        
+        return nil
+    }
 
-	if l1 != nil && l2 != nil {
+    resNode := &ListNode{}
+	res := co
+    
+    var l1Next *ListNode
+    var l2Next *ListNode
+
+	if l1 != nil {
 		res += l1.Val
+        l1Next = l1.Next
+	}
+    
+    if l2 != nil {
 		res += l2.Val
-	} else if l1 == nil {
-		res += l2.Val
-	} else if l2 == nil {
-		res += l1.Val
+        l2Next = l2.Next
 	}
 
 	if res > 9 {
-		return 1, res - 10
+        resNode.Val = res - 10
+        resNode.Next = addNode(1, l1Next, l2Next)
 	} else {
-		return 0, res
+		resNode.Val = res
+        resNode.Next = addNode(0, l1Next, l2Next)
 	}
+
+    return resNode
 }
