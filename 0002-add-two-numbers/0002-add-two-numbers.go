65d5aabc1a4d89801d37b97bfa13d995c634cd31
Samuel Adam Suhendra <samuel.suhendra@gmail.com>
2023-09-16 00:12:12 +0700

    Time: 15 ms (15.37%), Space: 4.5 MB (65.09%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.go
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -6,41 +6,24 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return addNode(0, l1, l2)
-}
-
-func addNode(co int, l1 *ListNode, l2 *ListNode) *ListNode {
-    if l1 == nil && l2 == nil {
-        if co == 1 {
-            return &ListNode{Val: 1}
+	    result := &ListNode{}
+    tmp := result
+    for l1 != nil || l2 != nil {
+        if l1 != nil {
+            tmp.Val += l1.Val
+            l1 = l1.Next
         }
-        
-        return nil
+        if l2 != nil {
+            tmp.Val += l2.Val
+            l2 = l2.Next
+        }
+        if tmp.Val > 9 {
+            tmp.Val -= 10
+            tmp.Next = &ListNode{Val: 1}
+        } else if l1 != nil || l2 != nil {
+            tmp.Next = &ListNode{}
+        }
+        tmp = tmp.Next
     }
-
-    resNode := &ListNode{}
-	res := co
-    
-    var l1Next *ListNode
-    var l2Next *ListNode
-
-	if l1 != nil {
-		res += l1.Val
-        l1Next = l1.Next
-	}
-    
-    if l2 != nil {
-		res += l2.Val
-        l2Next = l2.Next
-	}
-
-	if res > 9 {
-        resNode.Val = res - 10
-        resNode.Next = addNode(1, l1Next, l2Next)
-	} else {
-		resNode.Val = res
-        resNode.Next = addNode(0, l1Next, l2Next)
-	}
-
-    return resNode
+    return result
 }
