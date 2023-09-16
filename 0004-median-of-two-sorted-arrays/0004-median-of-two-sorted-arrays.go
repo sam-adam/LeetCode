f524cbeb951ea52d1036d48af65ee9e5d36c3d79
Samuel Adam Suhendra <samuel.suhendra@gmail.com>
2023-09-16 23:24:28 +0700

    Time: 14 ms (47.23%), Space: 5.1 MB (89.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
@@ -1,3 +1,4 @@
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	totalLen := len(nums1) + len(nums2)
 	isOdd := totalLen%2 == 1
@@ -7,11 +8,7 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	p1 := -1
 	p2 := -1
 
-	fmt.Println(fmt.Sprintf("nums1: %v", nums1))
-	fmt.Println(fmt.Sprintf("nums2: %v", nums2))
-
 	if isOdd {
-		fmt.Println("odd")
 		medianIdx := int(math.Floor(float64(totalLen) / 2.0))
 
 		for i := 0; i < medianIdx; i++ {
@@ -20,10 +17,8 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 			} else if p2+1 > lenNums2 {
 				p1++
 			} else if nums1[p1+1] <= nums2[p2+1] {
-				fmt.Println(fmt.Sprintf("p1: %d -> %d", p1, p1+1))
 				p1++
 			} else {
-				fmt.Println(fmt.Sprintf("p2: %d -> %d", p2, p2+1))
 				p2++
 			}
 		}
@@ -38,15 +33,10 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 			return float64(nums2[p2+1])
 		}
 	} else {
-		fmt.Println("even")
 		medianIdx := totalLen / 2
 		numPre := 0
 
-		fmt.Println(fmt.Sprintf("median-idx: %d", medianIdx))
-
 		for i := 0; i < medianIdx; i++ {
-			fmt.Println(fmt.Sprintf("i: %d, p1+1: %d, lenNums1: %d, p2+1: %d, lenNums2: %d", i, p1+1, lenNums1, p2+1, lenNums2))
-
 			if p1+1 > lenNums1 {
 				p2++
 				numPre = nums2[p2]
@@ -54,11 +44,9 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 				p1++
 				numPre = nums1[p1]
 			} else if nums1[p1+1] <= nums2[p2+1] {
-				fmt.Println(fmt.Sprintf("p1: %d -> %d", p1, p1+1))
 				p1++
 				numPre = nums1[p1]
 			} else {
-				fmt.Println(fmt.Sprintf("p2: %d -> %d", p2, p2+1))
 				p2++
 				numPre = nums2[p2]
 			}
