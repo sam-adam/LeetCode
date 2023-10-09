ca60db517940ec63072999a79d21cda07aca8305
Samuel Adam Suhendra <samuel.suhendra@gmail.com>
2023-10-09 22:57:31 +0700

    Time: 40 ms (5.58%), Space: 7.2 MB (5.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -0,0 +1,28 @@
+func isPalindrome(x int) bool {
+	strX := strconv.Itoa(x)
+	chars := make([]string, (2*len(strX))+1)
+
+	chars[len(chars)-1] = "#"
+
+	for _, c := range strX {
+		chars = append(chars, string(c), "#")
+	}
+
+	s1 := strings.Join(chars, "")
+	center := len(s1) / 2
+	left := center
+	right := center
+
+	fmt.Println(fmt.Sprintf("s = %s", s1))
+
+	for left > 0 {
+		if string(s1[left]) != string(s1[right]) {
+			return false
+		}
+
+		left--
+		right++
+	}
+
+	return true
+}
