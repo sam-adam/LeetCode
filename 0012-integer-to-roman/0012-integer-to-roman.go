296d63737eade6b69c1020ee0b5b6fb0e2a7f141
Samuel Adam Suhendra <samuel.suhendra@gmail.com>
2023-10-19 22:59:21 +0700

    Time: 4 ms (75.1%), Space: 3.2 MB (70.97%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.go
+++ 0012-integer-to-roman/0012-integer-to-roman.go
@@ -1,27 +1,69 @@
 func intToRoman(num int) string {
-	nums := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	numMap := map[int]string{
-		1000: "M",
-		900:  "CM",
-		500:  "D",
-		400:  "CD",
-		100:  "C",
-		90:   "XC",
-		50:   "L",
-		40:   "XL",
-		10:   "X",
-		9:    "IX",
-		5:    "V",
-		4:    "IV",
-		1:    "I",
-	}
-
 	result := ""
 
-	for _, i := range nums {
-		times := math.Floor(float64(num / i))
-		result = result + strings.Repeat(numMap[i], int(times))
-		num -= int(i * int(times))
+	for num >= 1000 {
+		result = result + "M"
+		num -= 1000
+	}
+
+	for num >= 900 {
+		result = result + "CM"
+		num -= 900
+	}
+
+	for num >= 500 {
+		result = result + "D"
+		num -= 500
+	}
+
+	for num >= 400 {
+		result = result + "CD"
+		num -= 400
+	}
+
+	for num >= 100 {
+		result = result + "C"
+		num -= 100
+	}
+
+	for num >= 90 {
+		result = result + "XC"
+		num -= 90
+	}
+
+	for num >= 50 {
+		result = result + "L"
+		num -= 50
+	}
+
+	for num >= 40 {
+		result = result + "XL"
+		num -= 40
+	}
+
+	for num >= 10 {
+		result = result + "X"
+		num -= 10
+	}
+
+	for num >= 9 {
+		result = result + "IX"
+		num -= 10
+	}
+
+	for num >= 5 {
+		result = result + "V"
+		num -= 5
+	}
+
+	for num >= 4 {
+		result = result + "IV"
+		num -= 4
+	}
+
+	for num >= 1 {
+		result = result + "I"
+		num--
 	}
 
 	return result
